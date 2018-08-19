45bc0c1614ada2fe766cbb8a5d66d4e0cfe53c9f
coggeshall2 <roncogg@hotmail.com>
2018-08-19 11:22:20 -0400

    working on auth

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -26,31 +26,7 @@ const onSignIn = function (event) {
     .then(ui.signInSuccess)
     .catch(ui.failure)
 }
-// const getFormFields = require('../../../lib/get-form-fields')
-//
-// const api = require('./api')
-// const ui = require('./ui')
-//
-// const onSignUp = function (event) {
-//   event.preventDefault()
-//   console.log('sign up ran!')
-//
-//   const data = getFormFields(this)
-//   api.signUp(data)
-//     .then(ui.signUpSuccess)
-//     .catch(ui.signUpFailure)
-// }
-//
-//
-// const onSignIn = function (event) {
-//   event.preventDefault()
-//   console.log('sign in ran!')
-//
-//   const data = getFormFields(this)
-//   api.signIn(data)
-//     .then(ui.signInSuccess)
-//     .catch(ui.signInFailure)
-// }
+
 
 const onChangePassword = function (event) {
   event.preventDefault()
